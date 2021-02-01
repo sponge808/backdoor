fc69c2630f7ad0d658dc416cfa2a3a3f15fc1c67
rabbitx1337 <72482820+rabbitx1337@users.noreply.github.com>
2021-02-01 16:15:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,11 +27,263 @@ class auth
 
 	public function displayLogin()
 	{
-		// https://codepen.io/linux/pen/jYMOBe
 		?>
-		<form method='post'>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="author" content="Syahrizaldev">
+		<style type="text/css">
+			:root {
+				--color-white: #ffffff;
+				--color-light: #f1f5f9;
+				--color-black: #121212;
+				--color-night: #001632;
+				--color-red: #f44336;
+				--color-blue: #1a73e8;
+				--color-gray: #80868b;
+				--color-grayish: #dadce0;
+				--shadow-normal: 0 1px 3px 0 rgba(0, 0, 0, 0.1),
+				0 1px 2px 0 rgba(0, 0, 0, 0.06);
+				--shadow-medium: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+				0 2px 4px -1px rgba(0, 0, 0, 0.06);
+				--shadow-large: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+				0 4px 6px -2px rgba(0, 0, 0, 0.05);
+			}
+
+			html {
+				font-size: 100%;
+				font-size-adjust: 100%;
+				box-sizing: border-box;
+				scroll-behavior: smooth;
+			}
+
+			*,
+			*::before,
+			*::after {
+				padding: 0;
+				margin: 0;
+				box-sizing: inherit;
+				list-style: none;
+				list-style-type: none;
+				text-decoration: none;
+				-webkit-font-smoothing: antialiased;
+				-moz-osx-font-smoothing: grayscale;
+				text-rendering: optimizeLegibility;
+			}
+
+			body {
+				font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+				font-size: 1rem;
+				font-weight: normal;
+				line-height: 1.5;
+				color: var(--color-black);
+				background: var(--color-light);
+			}
+
+			a,
+			button {
+				font-family: inherit;
+				font-size: inherit;
+				line-height: inherit;
+				cursor: pointer;
+				border: none;
+				outline: none;
+				background: none;
+				text-decoration: none;
+			}
+
+			img {
+				display: block;
+				width: 100%;
+				height: auto;
+				-o-object-fit: cover;
+				object-fit: cover;
+			}
+
+			.container {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				max-width: 80rem;
+				min-height: 100vh;
+				width: 100%;
+				padding: 0 2rem;
+				margin: 0 auto;
+			}
+
+			.ion-logo-apple {
+				font-size: 1.65rem;
+				line-height: inherit;
+				margin-right: 0.5rem;
+				color: var(--color-black);
+			}
+			.ion-logo-google {
+				font-size: 1.65rem;
+				line-height: inherit;
+				margin-right: 0.5rem;
+				color: var(--color-red);
+			}
+			.ion-logo-facebook {
+				font-size: 1.65rem;
+				line-height: inherit;
+				margin-right: 0.5rem;
+				color: var(--color-blue);
+			}
+
+			.text {
+				font-family: inherit;
+				line-height: inherit;
+				text-transform: unset;
+				text-rendering: optimizeLegibility;
+			}
+			.text-large {
+				font-size: 2rem;
+				font-weight: 600;
+				color: var(--color-black);
+			}
+			.text-normal {
+				font-size: 1rem;
+				font-weight: 400;
+				color: var(--color-black);
+			}
+			.text-links {
+				font-size: 1rem;
+				font-weight: 400;
+				color: var(--color-blue);
+			}
+			.text-links:hover {
+				text-decoration: underline;
+			}
+
+			.main .wrapper {
+				max-width: 28rem;
+				width: 100%;
+				margin: 2rem auto;
+				padding: 2rem 2.5rem;
+				border: none;
+				outline: none;
+				border-radius: 0.25rem;
+				color: var(--color-black);
+				background: var(--color-white);
+				box-shadow: var(--shadow-large);
+			}
+			.main .wrapper .form {
+				width: 100%;
+				height: auto;
+				margin-top: 2rem;
+			}
+			.main .wrapper .form .input-control {
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				margin-bottom: 1.25rem;
+			}
+			.main .wrapper .form .input-field {
+				font-family: inherit;
+				font-size: 1rem;
+				font-weight: 400;
+				line-height: inherit;
+				width: 100%;
+				height: auto;
+				padding: 0.75rem 1.25rem;
+				border: none;
+				outline: none;
+				border-radius: 2rem;
+				color: var(--color-black);
+				background: var(--color-light);
+				text-transform: unset;
+				text-rendering: optimizeLegibility;
+			}
+			.main .wrapper .form .input-submit {
+				font-family: inherit;
+				font-size: 1rem;
+				font-weight: 500;
+				line-height: inherit;
+				cursor: pointer;
+				min-width: 40%;
+				height: auto;
+				padding: 0.65rem 1.25rem;
+				border: none;
+				outline: none;
+				border-radius: 2rem;
+				color: var(--color-white);
+				background: var(--color-blue);
+				box-shadow: var(--shadow-medium);
+				text-transform: capitalize;
+				text-rendering: optimizeLegibility;
+			}
+			.main .wrapper .striped {
+				display: flex;
+				flex-direction: row;
+				justify-content: center;
+				align-items: center;
+				margin: 1rem 0;
+			}
+			.main .wrapper .striped-line {
+				flex: auto;
+				flex-basis: auto;
+				border: none;
+				outline: none;
+				height: 2px;
+				background: var(--color-grayish);
+			}
+			.main .wrapper .striped-text {
+				font-family: inherit;
+				font-size: 1rem;
+				font-weight: 500;
+				line-height: inherit;
+				color: var(--color-black);
+				margin: 0 1rem;
+			}
+			.main .wrapper .method-control {
+				margin-bottom: 1rem;
+			}
+			.main .wrapper .method-action {
+				font-family: inherit;
+				font-size: 0.95rem;
+				font-weight: 500;
+				line-height: inherit;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				width: 100%;
+				height: auto;
+				padding: 0.5rem 1.25rem;
+				outline: none;
+				border: 2px solid var(--color-grayish);
+				border-radius: 2rem;
+				color: var(--color-black);
+				background: var(--color-white);
+				text-transform: capitalize;
+				text-rendering: optimizeLegibility;
+				transition: all 0.35s ease;
+			}
+			.main .wrapper .method-action:hover {
+				background: var(--color-light);
+			}
+		</style>
+		<main class="main">
+			<div class="container">
+				<section class="wrapper">
+					<div class="heading">
+						<h1 class="text text-large">Login PHPFilemanager</h1>
+					</div>
+					<form method="post" class="form">
+						<div class="input-control">
+							<label for="password" class="input-label" hidden>Password</label>
+							<input type="password" name="pass" class="input-field" placeholder="Password">
+						</div>
+						<div class="input-control">
+							<a href="#" class="text text-links"></a>
+							<input type="submit"  class="input-submit">
+						</div>
+					</form>
+				</section>
+			</div>
+		</main>
+		<!-- <form method='post'>
 			<input type='password'name='pass'>
-		</form>
+		</form> -->
 		<?php
 	}
 
@@ -1387,6 +1639,10 @@ $Tools = new Tools;
 		outline: none;
 	}
 
+	.clickable:hover {
+		cursor: pointer;
+	}
+
 	div#nav {
 		position: relative;
 		justify-self: right;
@@ -1474,6 +1730,11 @@ $Tools = new Tools;
 	.back {
 		padding:px;
 		padding-left:15px;
+	}
+
+	.space {
+		width: 100%
+		background:red;
 	}
 
 	/*.back a {
@@ -1650,7 +1911,7 @@ $Tools = new Tools;
 <div class="grid">
 	<div class="nav">
 		<div class="content">
-			<h3 class="brand"><a href="<?= $listFiles->mySelf() ?>">PHPFilemanager</a></h3>
+			<h3 class="brand"><a href="/<?= $listFiles->mySelf() ?>">PHPFilemanager</a></h3>
 			<div id="nav">
 				<i class="dropdown-toggle material-icons menu">menu</i>
 				<ul class="dropdown">
@@ -1674,6 +1935,11 @@ $Tools = new Tools;
 							<span class="actions material-icons">filter_none</span> New File
 						</a>
 					</li>
+					<li>
+						<a href="?x=<?= hex::toHex(getcwd()) ?>&t=<?= hex::toHex("logout") ?>">
+							<span class="material-icons">logout</span> Logout
+						</a>
+					</li>
 				</ul>
 			</div>
 		</div>
@@ -1686,6 +1952,12 @@ $Tools = new Tools;
 		<?php
 		// Tools
 		switch (@$_GET['t']) {
+			// Logout
+			case hex::toHex("logout"):
+				$auth->logout();
+				break;
+			// end Logout
+
 			// Upload File
 			case hex::toHex("upload"):
 				print("Upload");
